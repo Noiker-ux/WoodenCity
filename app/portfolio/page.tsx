3f27e9c851b416c69436c03ceb9db21529fdbfdd
Noiker-UX <pasha_pasha_201@mail.ru>
2025-06-09 21:54:47 +0300

    new fix

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,135 +4,135 @@ export default function PortfolioPage() {
   return (
     <div className="mt-20">
       <h1 className="title">Наши работы</h1>
-      <div className="mt-10 grid grid-cols-4 gap-5">
+      <div className="mt-10 mx-auto grid grid-cols-1 md:grid-cols-4 gap-5">
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/1.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/2.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/3.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/4.jfif"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/5.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/6.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/7.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/8.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/9.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/10.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/11.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/12.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/13.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/14.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/15.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/17.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/18.webp"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/19.jpg"}
           alt="Object1"
           width={500}
           height={500}
         />
         <Image
-          className="w-96 h-96 object-cover"
+          className="w-full md:w-96 h-96 object-cover"
           src={"/Jobs/20.jpg"}
           alt="Object1"
           width={500}
--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -12,20 +12,11 @@ export default function Example() {
 
   return (
     <header className="relative z-20">
-      <nav
-        aria-label="Global"
-        className="flex items-center justify-between p-6 lg:px-8"
-      >
+      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <Image
-              src={"/logo.svg"}
-              width={270}
-              height={70}
-              alt="Логотип WoodenCity"
-              className="w-fit"
-            />
+            <Image src={"/logo.svg"} width={270} height={70} alt="Логотип WoodenCity" className="w-fit" />
           </Link>
         </div>
         <div className="flex lg:hidden">
@@ -54,22 +45,13 @@ export default function Example() {
         </div>
       </nav>
 
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[var(--second)] px-6 py-6 sm:max-w-sm sm:ring-1 sm:text-white">
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[var(--second)] px-6 py-6 sm:max-w-sm sm:ring-1 sm:text-white">
           <div className="flex items-center justify-between">
             <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">WoodenCity</span>
-              <Image
-                src={"/logoIcon.svg"}
-                width={50}
-                height={50}
-                alt="Логотип WoodenCity"
-              />
+              <Image src={"/logoIcon.svg"} width={50} height={50} alt="Логотип WoodenCity" />
             </Link>
             <button
               type="button"
@@ -88,6 +70,7 @@ export default function Example() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-[var(--text)]"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
@@ -95,12 +78,7 @@ export default function Example() {
               </div>
               <div className="py-6 flex gap-5 text-[var(--text)]">
                 <p>Musik und Effekte</p>
-                <Image
-                  src={"/Header/musicIcon.png"}
-                  width={40}
-                  height={23}
-                  alt="Музыка"
-                />
+                <Image src={"/Header/musicIcon.png"} width={40} height={23} alt="Музыка" />
               </div>
             </div>
           </div>
